372298f00e472faf4dc63dfbe3202d3258bd8852
Sahil Faraz <sahilfaraz420@gmail.com>
2026-05-01 02:14:20 +0500

    Delete config directory

--- config/cert_config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR  = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-CA_CERT     = os.path.join(BASE_DIR, "certificates", "ca.crt")
-SERVER_CERT = os.path.join(BASE_DIR, "certificates", "server.crt")
-SERVER_KEY  = os.path.join(BASE_DIR, "certificates", "server.key")
-CLIENT_CERT = os.path.join(BASE_DIR, "certificates", "client.crt")
-CLIENT_KEY  = os.path.join(BASE_DIR, "certificates", "client.key")
